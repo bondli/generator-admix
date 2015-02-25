200a9ecbc18afabd6ea77daa10ed1740cd96c33a
hongbang.lhb <hongbang.lhb@alibaba-inc.com>
2015-02-25 11:28:28 +0800

    增加模版代码

--- page/index.js
+++ page/index.js
@@ -12,18 +12,42 @@ var Generator = module.exports = function Generator() {
 util.inherits(Generator, ScriptBase);
 
 Generator.prototype.createPageFiles = function createPageFiles() {
+  
+  var htmlFile = 'page.html',
+    jsFile = 'page.js',
+    lessFile = 'page.less',
+
+    tmpName = this.name.toLowerCase();
+
+  if(tmpName.indexOf('list') > -1){ //list
+    htmlFile = 'list.html';
+    jsFile = 'list.js';
+    lessFile = 'list.less';
+  }
+  else if(tmpName.indexOf('detail') > -1){ //detail
+    htmlFile = 'detail.html';
+    jsFile = 'detail.js';
+    lessFile = 'detail.less';
+  }
+  else if(tmpName.indexOf('edit') > -1){ //edit
+    htmlFile = 'edit.html';
+    jsFile = 'edit.js';
+    lessFile = 'edit.less';
+  }
+
+
   this.htmlTemplate(
-    'page.html',
+    htmlFile,
     path.join(this.name + '.html')
   );
 
   this.appTemplate(
-    'page.js',
+    jsFile,
     path.join('pages', this.name, 'index.js')
   );
 
   this.appTemplate(
-    'page.less',
+    lessFile,
     path.join('pages', this.name, 'index.less')
   );
 };
--- templates/common/_README.md
+++ templates/common/_README.md
@@ -36,3 +36,17 @@ grunt build
 ```bash
 bower install git://github.com/bondli/admix-ui.git#daily/0.0.1
 ```
+
+### 交互规范
+
+* 1、前端校验错误统一是toast提示；
+* 2、后端校验错误统一是modal.alert框提示，一般不会出现，除非提交时接口有定义校验出错；
+* 3、后端成功是tips.show({type:’success’})，失败是tips.show({type:’error’})；
+* 4、数据删除前统一给出modal.confirm()提示，确认后才删除；
+* 5、列表页下拉触顶时出现搜索框，上拉时隐藏，ios没有搜索按钮，android是有的，提交搜索是到搜索页面；
+6、搜索页面中默认就有搜索框，并且带过来搜索条件，提交时需要把location.replace掉；
+7、页面无数据，或者查询失败，调用setNodata(true); 并且给错toast提示即可；
+8、所有页面发接口请求必须有loading.show()；
+9、提交数据保存的按钮一定要做防止快速双击处理，避免数据多次提交；
+10、页面操过一屏的单个操作按钮是吸底的，一直要出现；
+11、tab切换的都是吸顶，一直要出现；
